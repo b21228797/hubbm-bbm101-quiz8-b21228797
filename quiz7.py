c50ebba4f32f781c468f2ee92d80fb34e2546b39
b21228797 <32814734+b21228797@users.noreply.github.com>
2017-12-08 21:34:08 +0300

    Add files via upload

--- quiz7.py
+++ quiz7.py
@@ -1,40 +1,34 @@
-'''
-This starter code provides you the expected message formats 
-'''
+try:
+    assert False, "Hello!"
+except AssertionError as e:
+    e.args += ('some other', 'important', 'information', 42)
+    raise
+  try:
+    z = x / y
+except ZeroDivisionError:
+    z = 0
+    n = 0
+y = len(list1)-1
+found = 0
+while n < y:
+    for k in list1:
+        if list1[n]+ k == g:
+            print("The 2 prime numbers that add up to ",g,"are ", list1[n]," and ",k,".")
+            found = 1
+            break # for loop
+    if found:
+       break # while loop
+    n = n + 1
+    try:
+    ...
+except SomeException:
+    tb = sys.exc_info()[2]
+    raise OtherException(...).with_traceback(tb)
+    def lines_to_dict(lines):
+    return_dict = {}
+    for ll in lines:
 
-#Separator for each line of calculation (should be inserted at the top!):
-print("------------")
-    
-#IOError message:
-print("IOError: cannot open", filename_that_caused_trouble)
+            [key, value] = ll.split()
+            return_dict[key] = value
 
-#IndexError message for missing file names:
-print("IndexError: number of input files less than expected.")
-
-# ValueError  message:
-print("ValueError: only numeric input is accepted.")
-print("Given input:", given_input_that_caused_trouble)
-
-# IndexError message for missing operands:
-print("IndexError: number of operands less than expected.")
-print("Given input:", given_input_that_caused_trouble)
-
-# ZeroDivisionError message:
-print("ZeroDivisionError: You can't divide by 0.")
-print("Given input:", given_input_that_caused_trouble)
-
-# Comparing results messages:
-print("My results:\t\t", your_results)
-print("Results to compare:\t", given_comparison_data)
-
-# AssertionError message for result mismatch:
-print("Assertion Error: results don't match.")
-
-# Message for result match:
-print("Goool!!!")
-
-# Error message for any other exception:
-print("kaBOOM: run for your life!")
-
-# Message for terminating the program:
-print("\n~ Game Over ~")
+    return return_dict
